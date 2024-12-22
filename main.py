41ff903abff69cc6edc84efee12277fe75412b92
Bora Arseven <bora.arseven@gmail.com>
2024-12-22 19:20:12 +0100

    numpy

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #%%
 # Import libraries
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 #%%
 # Defining data for the dataframe
 data = {
-    'Basket': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'],
-    'Apples': [10, 20, 30, 56, 40, 40, 67, 47, 40, 4, 49, 52, 5, 56, 35, 45],
-    'Bananas': [15, 6, 3, 45, 67, 44, 45, 11, 14, 18, 13, 12, 1, 34, 12, 12]
+    'Basket': np.array(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']),
+    'Apples': np.array([10, 20, 30, 56, 40, 40, 67, 47, 40, 4, 49, 52, 5, 56, 35, 45]),
+    'Bananas': np.array([15, 6, 3, 45, 67, 44, 45, 11, 14, 18, 13, 12, 1, 34, 12, 12])
 }
 
 # Creating the dataframe
